ebca6496a2454a5bd2e98306b99e2d6be474ab7a
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2024-12-05 16:50:45 +0100

    Added the beginings of map generation and fixed World1_StartingRoom having the RoomDescriptor set up incorrectly.

---
+++ Unity/Assets/Scripts/World Generation/WorldBuilder.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WorldBuilder : MonoBehaviour
+{
+    public GameObject StartingRoom;
+    void Start()
+    {
+        Instantiate(StartingRoom, Vector3.zero, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}

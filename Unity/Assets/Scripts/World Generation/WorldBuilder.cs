6750e31b5592d63f370a9c1a58f2dccfb8e9eead
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2024-12-06 19:38:57 +0100

    Moved World1 prefabs from Assets/Prefabs to Assets/Resources/Prefabs.

--- Unity/Assets/Scripts/World Generation/WorldBuilder.cs
+++ Unity/Assets/Scripts/World Generation/WorldBuilder.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour
 {
     public GameObject StartingRoom;
+    public List<GameObject> RoomPrefabs;
+
     void Start()
     {
+        GameObject[] folderObjects = Resources.LoadAll<GameObject>("Prefabs/World1");
+
         Instantiate(StartingRoom, Vector3.zero, Quaternion.identity);
+
+        foreach (GameObject i in folderObjects)
+        {
+            RoomPrefabs.Add(i);
+        }
+
+        int numberOfRoomPrefabs = RoomPrefabs.Count;
+
+        if (numberOfRoomPrefabs == 0)
+        {
+            Debug.Log("No prefab rooms were found at the specified location.");
+        }
+        else
+        {
+            Debug.Log("The number of room prefabs: " +  numberOfRoomPrefabs);
+        }
     }
 }

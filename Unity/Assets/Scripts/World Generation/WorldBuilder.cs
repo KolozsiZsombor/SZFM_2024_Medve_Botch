f0701d1e32dcc2fc2e709bbfe56a0fed526f6b06
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2024-12-07 12:50:19 +0100

    Added very basic level generation.

--- Unity/Assets/Scripts/World Generation/WorldBuilder.cs
+++ Unity/Assets/Scripts/World Generation/WorldBuilder.cs
@@ -1,34 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngineInternal;
 
 public class WorldBuilder : MonoBehaviour
 {
     public GameObject StartingRoom;
     public List<GameObject> RoomPrefabs;
+    public int RoomsThisLevel = 4;
+    public RoomDescriptor script;
+    private Vector3 FreeLeftAttachment;
+    private Vector3 FreeRightAttachment;
 
     void Start()
     {
         GameObject[] folderObjects = Resources.LoadAll<GameObject>("Prefabs/World1");
 
-        Instantiate(StartingRoom, Vector3.zero, Quaternion.identity);
-
+        // Add all of the GameObjects found in "Prefabs/World1 to a list."
         foreach (GameObject i in folderObjects)
         {
             RoomPrefabs.Add(i);
         }
 
+        Instantiate(StartingRoom, Vector3.zero, Quaternion.identity);
+
+        FreeLeftAttachment = StartingRoom.transform.Find("LeftAttachment").transform.position;
+        FreeRightAttachment = StartingRoom.transform.Find("RightAttachment").transform.position;
+
+
+        // We remove the Starting Room from the list of all prefabs to avoid spawning it.
+        RoomPrefabs.Remove(StartingRoom);
+
         int numberOfRoomPrefabs = RoomPrefabs.Count;
 
-        if (numberOfRoomPrefabs == 0)
+        while (RoomsThisLevel > 0)
         {
-            Debug.Log("No prefab rooms were found at the specified location.");
+            GameObject SelectedRoom = RoomPrefabs[UnityEngine.Random.Range(0, numberOfRoomPrefabs)];
+            PlacePrefab(SelectedRoom, FreeRightAttachment, true);
+            RoomsThisLevel--;
+        }
+
+
+    }
+
+    /// <summary>
+    /// Instantiates a given prefab at the given  attachment point,
+    /// updates the location of the free attachment points.
+    /// </summary>>
+    /// <param name="roomPrefab">The prefab to instantiate.</param> 
+    /// <param name="AttacherPosition">The attacher to place the prefab on.</param> 
+    /// <param name="AttachmentSide">The side where the given attachment point is. False for left and True for right.</param> 
+    public void PlacePrefab(GameObject roomPrefab, Vector3 AttacherPosition, bool AttachmentSide)
+    {
+        Vector3 LeftAttachment = roomPrefab.transform.Find("LeftAttachment").transform.position;
+        Vector3 RightAttachment = roomPrefab.transform.Find("RightAttachment").transform.position;
+
+        Vector3 PlacePoint = Vector3.zero;
+
+        // If the attachment point is on the left.
+        if (AttachmentSide == false)
+        {
+            PlacePoint.y = AttacherPosition.y + (RightAttachment.y * -1);
+            PlacePoint.x = AttacherPosition.x + (RightAttachment.x * -1);
+            Instantiate(roomPrefab, PlacePoint, Quaternion.identity);
         }
         else
         {
-            Debug.Log("The number of room prefabs: " +  numberOfRoomPrefabs);
+            PlacePoint.y = AttacherPosition.y + (LeftAttachment.y * -1);
+            PlacePoint.x = AttacherPosition.x + (LeftAttachment.x * -1);
+            Instantiate(roomPrefab, PlacePoint, Quaternion.identity);
         }
+        script = FindObjectOfType<RoomDescriptor>();
+        FreeLeftAttachment = script.GetLeftPosition();
+        FreeRightAttachment = script.GetRightPosition();
     }
 }

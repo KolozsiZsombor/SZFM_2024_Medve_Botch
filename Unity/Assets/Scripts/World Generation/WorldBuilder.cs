fcec88cbf46d95bd781b73796ea8d2c81be8d34d
Kolozsi Zsombor <kolozsizsombor1@gmail.com>
2024-12-07 21:50:29 +0100

    Added more advanced functionality to WorldBuilder.

--- Unity/Assets/Scripts/World Generation/WorldBuilder.cs
+++ Unity/Assets/Scripts/World Generation/WorldBuilder.cs
@@ -11,11 +11,22 @@ using UnityEngineInternal;
 public class WorldBuilder : MonoBehaviour
 {
     public GameObject StartingRoom;
+    public GameObject EndingRoom;
+    public GameObject ShopRoom;
+    public GameObject GiftRoom;
     public List<GameObject> RoomPrefabs;
-    public int RoomsThisLevel = 4;
+    public int MinimumRoomsPerLevel = 4;
+    public int MaximumRoomsPerLevel = 8;
     public RoomDescriptor script;
+    private bool ShopRoomPlaced = false;
+    private bool GiftRoomPlaced = false;
+    public int CurrentLevel = 1;
+    public int CurrentWorld = 1;
+    private int RoomsPlaced = 0;
     private Vector3 FreeLeftAttachment;
     private Vector3 FreeRightAttachment;
+    public int SideToPlaceOn;
+    public int NumberOfRoomPrefabs;
 
     void Start()
     {
@@ -35,13 +46,44 @@ public class WorldBuilder : MonoBehaviour
         // We remove the Starting Room from the list of all prefabs to avoid spawning it.
         RoomPrefabs.Remove(StartingRoom);
 
+        // The amount of rooms we can choose from.
         int numberOfRoomPrefabs = RoomPrefabs.Count;
+        // Roll a random number for deciding how many rooms we are going to have this level.
+        int RoomsThisLevel = UnityEngine.Random.Range(MinimumRoomsPerLevel, MaximumRoomsPerLevel);
 
-        while (RoomsThisLevel > 0)
+        // The main loop for creating the level.
+        while (RoomsThisLevel >= RoomsPlaced)
         {
+            int SideToPlaceOn = UnityEngine.Random.Range(0, 2);
             GameObject SelectedRoom = RoomPrefabs[UnityEngine.Random.Range(0, numberOfRoomPrefabs)];
-            PlacePrefab(SelectedRoom, UnityEngine.Random.Range(0, 2));
-            RoomsThisLevel--;
+
+            // Deciding what to do based on the room selected, since some rooms require special treatment.
+            // If we randomly selected the shop room.
+            if (SelectedRoom == ShopRoom)
+            {
+                PlaceShopRoom();
+            }
+            // If we placed more than a third of all rooms this level and havent placed a shop yet.
+            else if ((RoomsPlaced > RoomsThisLevel / 3) && !ShopRoomPlaced)
+            {
+                PlaceShopRoom();
+            }
+            // If we randomly selected the gift room.
+            else if (SelectedRoom == GiftRoom)
+            {
+                PlaceGiftRoom();
+            }
+            // If we placed more than half of all rooms this level and havent placed a gift room yet.
+            else if ((RoomsPlaced > RoomsThisLevel / 2) && !GiftRoomPlaced)
+            {
+                PlaceGiftRoom();
+            }
+            // If we selected a normal room.
+            else
+            {
+                PlacePrefab(SelectedRoom, SideToPlaceOn);
+                RoomsPlaced++;
+            }
         }
     }
 
@@ -77,5 +119,33 @@ public class WorldBuilder : MonoBehaviour
             FreeRightAttachment = script.GetRightPosition();
 
         }
+    }
+
+    /// <summary>
+    /// Special function for placing the shop room.
+    /// It places the shop then removes it from the list of spawnable rooms.
+    /// Updates the RoomsPlaced and NumberOfRoomPrefabs variables.
+    /// </summary>
+    public void PlaceShopRoom()
+    {
+        PlacePrefab(ShopRoom, SideToPlaceOn);
+        RoomPrefabs.Remove(ShopRoom);
+        ShopRoomPlaced = true;
+        RoomsPlaced++;
+        NumberOfRoomPrefabs--;
+    }
+
+    /// <summary>
+    /// Special function for placing the gift room.
+    /// It places the gift room then removes it from the list of spawnable rooms.
+    /// Updates the RoomsPlaced and NumberOfRoomPrefabs variables.
+    /// </summary>
+    public void PlaceGiftRoom()
+    {
+        PlacePrefab(GiftRoom, SideToPlaceOn);
+        RoomPrefabs.Remove(GiftRoom);
+        GiftRoomPlaced = true;
+        RoomsPlaced++;
+        NumberOfRoomPrefabs--;
     }
 }
